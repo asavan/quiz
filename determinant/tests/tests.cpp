054d412eeac9cd162b3d253ead5bbbcc22ed58fb
a.savanovich <asavanster@gmail.com>
2020-04-16 10:57:10 +0300

    add tests

---
+++ determinant/tests/tests.cpp
@@ -0,0 +1,24 @@
+#include "../solver/solver.h"
+#include <cassert>
+int main()
+{
+
+    std::vector<std::vector<int>> matrix = {
+      {0, 0, 0},
+      {0, 5, 0},
+      {0, 0, 0}
+    };
+
+    BestResult res = solve_matrix(matrix);
+    assert(res.result == 40);
+
+    matrix = {
+      {0, 0, 0},
+      {0, 3, 0},
+      {0, 0, 0}
+    };
+
+    res = solve_matrix(matrix);
+    assert(res.result == -9);
+    return 0;
+}
