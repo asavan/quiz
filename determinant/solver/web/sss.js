c6b0a6287890c3b9ebd924e329c311a6f198ca6a
a.savanovich <asavanster@gmail.com>
2020-07-18 22:37:54 +0300

    add wasm c and cpp

---
+++ determinant/solver/web/sss.js
@@ -0,0 +1,433 @@
+var Module = typeof Module !== "undefined" ? Module : {};
+var moduleOverrides = {};
+var key;
+for (key in Module) {
+    if (Module.hasOwnProperty(key)) {
+        moduleOverrides[key] = Module[key]
+    }
+}
+var arguments_ = [];
+var thisProgram = "./this.program";
+var quit_ = function (status, toThrow) {
+    throw toThrow
+};
+var ENVIRONMENT_IS_WEB = true;
+var ENVIRONMENT_IS_WORKER = false;
+var scriptDirectory = "";
+
+function locateFile(path) {
+    if (Module["locateFile"]) {
+        return Module["locateFile"](path, scriptDirectory)
+    }
+    return scriptDirectory + path
+}
+
+var read_, readAsync, readBinary, setWindowTitle;
+if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
+    if (ENVIRONMENT_IS_WORKER) {
+        scriptDirectory = self.location.href
+    } else if (document.currentScript) {
+        scriptDirectory = document.currentScript.src
+    }
+    if (scriptDirectory.indexOf("blob:") !== 0) {
+        scriptDirectory = scriptDirectory.substr(0, scriptDirectory.lastIndexOf("/") + 1)
+    } else {
+        scriptDirectory = ""
+    }
+    {
+        read_ = function shell_read(url) {
+            var xhr = new XMLHttpRequest;
+            xhr.open("GET", url, false);
+            xhr.send(null);
+            return xhr.responseText
+        };
+        if (ENVIRONMENT_IS_WORKER) {
+            readBinary = function readBinary(url) {
+                var xhr = new XMLHttpRequest;
+                xhr.open("GET", url, false);
+                xhr.responseType = "arraybuffer";
+                xhr.send(null);
+                return new Uint8Array(xhr.response)
+            }
+        }
+        readAsync = function readAsync(url, onload, onerror) {
+            var xhr = new XMLHttpRequest;
+            xhr.open("GET", url, true);
+            xhr.responseType = "arraybuffer";
+            xhr.onload = function xhr_onload() {
+                if (xhr.status == 200 || xhr.status == 0 && xhr.response) {
+                    onload(xhr.response);
+                    return
+                }
+                onerror()
+            };
+            xhr.onerror = onerror;
+            xhr.send(null)
+        }
+    }
+    setWindowTitle = function (title) {
+        document.title = title
+    }
+} else {
+}
+var out = Module["print"] || console.log.bind(console);
+var err = Module["printErr"] || console.warn.bind(console);
+for (key in moduleOverrides) {
+    if (moduleOverrides.hasOwnProperty(key)) {
+        Module[key] = moduleOverrides[key]
+    }
+}
+moduleOverrides = null;
+if (Module["arguments"]) arguments_ = Module["arguments"];
+if (Module["thisProgram"]) thisProgram = Module["thisProgram"];
+if (Module["quit"]) quit_ = Module["quit"];
+var wasmBinary;
+if (Module["wasmBinary"]) wasmBinary = Module["wasmBinary"];
+var noExitRuntime;
+if (Module["noExitRuntime"]) noExitRuntime = Module["noExitRuntime"];
+if (typeof WebAssembly !== "object") {
+    abort("no native wasm support detected")
+}
+var wasmMemory;
+var wasmTable = new WebAssembly.Table({"initial": 2, "maximum": 2 + 0, "element": "anyfunc"});
+var ABORT = false;
+var EXITSTATUS = 0;
+var buffer, HEAP8, HEAPU8, HEAP16, HEAPU16, HEAP32, HEAPU32, HEAPF32, HEAPF64;
+
+function updateGlobalBufferAndViews(buf) {
+    buffer = buf;
+    Module["HEAP8"] = HEAP8 = new Int8Array(buf);
+    Module["HEAP16"] = HEAP16 = new Int16Array(buf);
+    Module["HEAP32"] = HEAP32 = new Int32Array(buf);
+    Module["HEAPU8"] = HEAPU8 = new Uint8Array(buf);
+    Module["HEAPU16"] = HEAPU16 = new Uint16Array(buf);
+    Module["HEAPU32"] = HEAPU32 = new Uint32Array(buf);
+    Module["HEAPF32"] = HEAPF32 = new Float32Array(buf);
+    Module["HEAPF64"] = HEAPF64 = new Float64Array(buf)
+}
+
+var INITIAL_INITIAL_MEMORY = Module["INITIAL_MEMORY"] || 16777216;
+
+function callRuntimeCallbacks(callbacks) {
+    while (callbacks.length > 0) {
+        var callback = callbacks.shift();
+        if (typeof callback == "function") {
+            callback(Module);
+            continue
+        }
+        var func = callback.func;
+        if (typeof func === "number") {
+            if (callback.arg === undefined) {
+                Module["dynCall_v"](func)
+            } else {
+                Module["dynCall_vi"](func, callback.arg)
+            }
+        } else {
+            func(callback.arg === undefined ? null : callback.arg)
+        }
+    }
+}
+
+var __ATPRERUN__ = [];
+var __ATINIT__ = [];
+var __ATMAIN__ = [];
+var __ATEXIT__ = [];
+var __ATPOSTRUN__ = [];
+var runtimeInitialized = false;
+var runtimeExited = false;
+
+function preRun() {
+    if (Module["preRun"]) {
+        if (typeof Module["preRun"] == "function") Module["preRun"] = [Module["preRun"]];
+        while (Module["preRun"].length) {
+            addOnPreRun(Module["preRun"].shift())
+        }
+    }
+    callRuntimeCallbacks(__ATPRERUN__)
+}
+
+function initRuntime() {
+    runtimeInitialized = true;
+    callRuntimeCallbacks(__ATINIT__)
+}
+
+function preMain() {
+    callRuntimeCallbacks(__ATMAIN__)
+}
+
+function exitRuntime() {
+    callRuntimeCallbacks(__ATEXIT__);
+    runtimeExited = true
+}
+
+function postRun() {
+    if (Module["postRun"]) {
+        if (typeof Module["postRun"] == "function") Module["postRun"] = [Module["postRun"]];
+        while (Module["postRun"].length) {
+            addOnPostRun(Module["postRun"].shift())
+        }
+    }
+    callRuntimeCallbacks(__ATPOSTRUN__)
+}
+
+function addOnPreRun(cb) {
+    __ATPRERUN__.unshift(cb)
+}
+
+function addOnPostRun(cb) {
+    __ATPOSTRUN__.unshift(cb)
+}
+
+var runDependencies = 0;
+var runDependencyWatcher = null;
+var dependenciesFulfilled = null;
+
+function addRunDependency(id) {
+    runDependencies++;
+    if (Module["monitorRunDependencies"]) {
+        Module["monitorRunDependencies"](runDependencies)
+    }
+}
+
+function removeRunDependency(id) {
+    runDependencies--;
+    if (Module["monitorRunDependencies"]) {
+        Module["monitorRunDependencies"](runDependencies)
+    }
+    if (runDependencies == 0) {
+        if (runDependencyWatcher !== null) {
+            clearInterval(runDependencyWatcher);
+            runDependencyWatcher = null
+        }
+        if (dependenciesFulfilled) {
+            var callback = dependenciesFulfilled;
+            dependenciesFulfilled = null;
+            callback()
+        }
+    }
+}
+
+Module["preloadedImages"] = {};
+Module["preloadedAudios"] = {};
+
+function abort(what) {
+    if (Module["onAbort"]) {
+        Module["onAbort"](what)
+    }
+    what += "";
+    out(what);
+    err(what);
+    ABORT = true;
+    EXITSTATUS = 1;
+    what = "abort(" + what + "). Build with -s ASSERTIONS=1 for more info.";
+    throw new WebAssembly.RuntimeError(what)
+}
+
+function hasPrefix(str, prefix) {
+    return String.prototype.startsWith ? str.startsWith(prefix) : str.indexOf(prefix) === 0
+}
+
+var dataURIPrefix = "data:application/octet-stream;base64,";
+
+function isDataURI(filename) {
+    return hasPrefix(filename, dataURIPrefix)
+}
+
+var wasmBinaryFile = "solve_web7.wasm";
+if (!isDataURI(wasmBinaryFile)) {
+    wasmBinaryFile = locateFile(wasmBinaryFile)
+}
+
+function getBinary() {
+    try {
+        if (wasmBinary) {
+            return new Uint8Array(wasmBinary)
+        }
+        if (readBinary) {
+            return readBinary(wasmBinaryFile)
+        } else {
+            throw"both async and sync fetching of the wasm failed"
+        }
+    } catch (err) {
+        abort(err)
+    }
+}
+
+function getBinaryPromise() {
+    if (!wasmBinary && (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) && typeof fetch === "function") {
+        return fetch(wasmBinaryFile, {credentials: "same-origin"}).then(function (response) {
+            if (!response["ok"]) {
+                throw"failed to load wasm binary file at '" + wasmBinaryFile + "'"
+            }
+            return response["arrayBuffer"]()
+        }).catch(function () {
+            return getBinary()
+        })
+    }
+    return new Promise(function (resolve, reject) {
+        resolve(getBinary())
+    })
+}
+
+function createWasm() {
+    var info = {"env": asmLibraryArg, "wasi_snapshot_preview1": asmLibraryArg};
+
+    function receiveInstance(instance, module) {
+        var exports = instance.exports;
+        Module["asm"] = exports;
+        wasmMemory = exports["memory"];
+        updateGlobalBufferAndViews(wasmMemory.buffer);
+        removeRunDependency("wasm-instantiate")
+    }
+
+    addRunDependency("wasm-instantiate");
+
+    function receiveInstantiatedSource(output) {
+        receiveInstance(output["instance"])
+    }
+
+    function instantiateArrayBuffer(receiver) {
+        return getBinaryPromise().then(function (binary) {
+            return WebAssembly.instantiate(binary, info)
+        }).then(receiver, function (reason) {
+            err("failed to asynchronously prepare wasm: " + reason);
+            abort(reason)
+        })
+    }
+
+    function instantiateAsync() {
+        if (!wasmBinary && typeof WebAssembly.instantiateStreaming === "function" && !isDataURI(wasmBinaryFile) && typeof fetch === "function") {
+            fetch(wasmBinaryFile, {credentials: "same-origin"}).then(function (response) {
+                var result = WebAssembly.instantiateStreaming(response, info);
+                return result.then(receiveInstantiatedSource, function (reason) {
+                    err("wasm streaming compile failed: " + reason);
+                    err("falling back to ArrayBuffer instantiation");
+                    return instantiateArrayBuffer(receiveInstantiatedSource)
+                })
+            })
+        } else {
+            return instantiateArrayBuffer(receiveInstantiatedSource)
+        }
+    }
+
+    if (Module["instantiateWasm"]) {
+        try {
+            var exports = Module["instantiateWasm"](info, receiveInstance);
+            return exports
+        } catch (e) {
+            err("Module.instantiateWasm callback failed with error: " + e);
+            return false
+        }
+    }
+    instantiateAsync();
+    return {}
+}
+
+__ATINIT__.push();
+var asmLibraryArg = {};
+var asm = createWasm();
+var _solve_matrix_web = Module["_solve_matrix_web"] = function () {
+    return (_solve_matrix_web = Module["_solve_matrix_web"] = Module["asm"]["solve_matrix_web"]).apply(null, arguments)
+};
+var __initialize = Module["__initialize"] = function () {
+    return (__initialize = Module["__initialize"] = Module["asm"]["_initialize"]).apply(null, arguments)
+};
+var calledRun;
+
+function ExitStatus(status) {
+    this.name = "ExitStatus";
+    this.message = "Program terminated with exit(" + status + ")";
+    this.status = status
+}
+
+var calledMain = false;
+var mainArgs = undefined;
+dependenciesFulfilled = function runCaller() {
+    if (!calledRun) run();
+    if (!calledRun) dependenciesFulfilled = runCaller
+};
+
+function callMain(args) {
+    var entryFunction = Module["__initialize"];
+    mainArgs = [thisProgram].concat(args);
+    try {
+        entryFunction();
+        var ret = 0;
+        exit(ret, true)
+    } catch (e) {
+        if (e instanceof ExitStatus) {
+            return
+        } else if (e == "unwind") {
+            noExitRuntime = true;
+            return
+        } else {
+            var toLog = e;
+            if (e && typeof e === "object" && e.stack) {
+                toLog = [e, e.stack]
+            }
+            err("exception thrown: " + toLog);
+            quit_(1, e)
+        }
+    } finally {
+        calledMain = true
+    }
+}
+
+function run(args) {
+    args = args || arguments_;
+    if (runDependencies > 0) {
+        return
+    }
+    preRun();
+    if (runDependencies > 0) return;
+
+    function doRun() {
+        if (calledRun) return;
+        calledRun = true;
+        Module["calledRun"] = true;
+        if (ABORT) return;
+        initRuntime();
+        preMain();
+        if (Module["onRuntimeInitialized"]) Module["onRuntimeInitialized"]();
+        if (shouldRunNow) callMain(args);
+        postRun()
+    }
+
+    if (Module["setStatus"]) {
+        Module["setStatus"]("Running...");
+        setTimeout(function () {
+            setTimeout(function () {
+                Module["setStatus"]("")
+            }, 1);
+            doRun()
+        }, 1)
+    } else {
+        doRun()
+    }
+}
+
+Module["run"] = run;
+
+function exit(status, implicit) {
+    if (implicit && noExitRuntime && status === 0) {
+        return
+    }
+    if (noExitRuntime) {
+    } else {
+        ABORT = true;
+        EXITSTATUS = status;
+        exitRuntime();
+        if (Module["onExit"]) Module["onExit"](status)
+    }
+    quit_(status, new ExitStatus(status))
+}
+
+if (Module["preInit"]) {
+    if (typeof Module["preInit"] == "function") Module["preInit"] = [Module["preInit"]];
+    while (Module["preInit"].length > 0) {
+        Module["preInit"].pop()()
+    }
+}
+var shouldRunNow = true;
+if (Module["noInitialRun"]) shouldRunNow = false;
+run();
