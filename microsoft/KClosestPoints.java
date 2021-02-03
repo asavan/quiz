6c3e0bea71cc4d8bd2a80fb7c941dac7f09a0e8c
a.savanovich <asavanster@gmail.com>
2021-02-03 20:29:49 +0300

    k closest points

---
+++ microsoft/KClosestPoints.java
@@ -0,0 +1,64 @@
+package microsoft;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * https://www.youtube.com/watch?v=581GAbMZUOA
+ */
+public class KClosestPoints {
+
+    static class Point {
+        public int x;
+        public int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int squareDistance(Point other) {
+            return square(x-other.x) + square(y-other.y);
+        }
+        private int square(int x) {
+            return x*x;
+        }
+
+        @Override
+        public String toString() {
+            return "( " + x + ", " + y + " )";
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Point> input = new ArrayList<>();
+        input.add(new Point(1, 1));
+        input.add(new Point(1, -1));
+        input.add(new Point(2, 2));
+        input.add(new Point(-1, 1));
+        input.add(new Point(-2, 2));
+        input.add(new Point(5, -5));
+        input.add(new Point(-8, 6));
+        input.add(new Point(0, 20));
+        input.add(new Point(0, 0));
+
+        Point vertex = new Point(0, 0);
+        List<Point> closest = kClosest(input, vertex, 3);
+        for (Point point : closest) {
+            System.out.println(point);
+        }
+    }
+
+    private static List<Point> kClosest(List<Point> input, Point vertex, int k) {
+        PriorityQueue<Point> queue = new PriorityQueue<>(k+1, (o1, o2) -> o2.squareDistance(vertex) - o1.squareDistance(vertex));
+        for (Point point : input) {
+            queue.add(point);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return new ArrayList<>(queue);
+    }
+
+}
