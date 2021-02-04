b3d05a74e5a9d9532dc009c82b9f189986a029a7
a.savanovich <asavanster@gmail.com>
2021-02-04 18:31:16 +0300

    beautify substring annagramm

--- ya/SubstringAnnagramm.java
+++ ya/SubstringAnnagramm.java
@@ -1,23 +1,40 @@
 package ya;
 
+import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by asavan on 04.02.2021.
  */
 public class SubstringAnnagramm {
     public static void main(String[] args) {
-        int index = findSubstring("tata", "ata");
+        int index = findSubstring("datav", "ata");
         System.out.println(index);
+    }
+
+    @Test
+    public void test() {
+        assertEquals(1, findSubstring("datav", "ata"));
+        assertEquals(0, findSubstring("atav", "ata"));
+        assertEquals(0, findSubstring("aatav", "ata"));
+        assertEquals(-1, findSubstring("aa", "ata"));
+        assertEquals(1, findSubstring("tata", "ata"));
+        assertEquals(1, findSubstring("baata", "ata"));
+        assertEquals(0, findSubstring("taata", "ata"));
     }
 
     public static int findSubstring(String t, String s) {
         int res = -1;
-        Map<Character, Integer> freq = new HashMap<>();
         if (t.length() < s.length()) {
             return res;
         }
+        Map<Character, Integer> freq = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             char a = s.charAt(i);
             Integer prev = freq.get(a);
@@ -29,19 +46,13 @@ public class SubstringAnnagramm {
         }
 
         int zeroes = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char a = t.charAt(i);
-            zeroes += addChar(freq, a);
-        }
-        if (zeroes == freq.size()) {
-            res = 0;
-            return res;
-        }
-        for (int i = s.length(); i < t.length(); i++) {
-            char out = t.charAt(i - s.length());
+        for (int i = 0; i < t.length(); i++) {
             char in = t.charAt(i);
-            zeroes += removeChar(freq, zeroes, out);
-            zeroes += addChar(freq, in);
+            zeroes += addChar(freq, in, p -> p - 1);
+            if (i >= s.length()) {
+                char out = t.charAt(i - s.length());
+                zeroes += addChar(freq, out, p -> p + 1);
+            }
             if (zeroes == freq.size()) {
                 res = i - s.length() + 1;
                 break;
@@ -50,27 +61,15 @@ public class SubstringAnnagramm {
         return res;
     }
 
-    private static int removeChar(Map<Character, Integer> freq, int zeroes, char out) {
-        Integer prev = freq.get(out);
-        if (prev != null) {
-            freq.put(out, prev + 1);
-            if (prev + 1 == 0) {
-                ++zeroes;
-            } else if (prev == 0) {
-                --zeroes;
-            }
-        }
-        return zeroes;
-    }
-
-    private static int addChar(Map<Character, Integer> freq, char a) {
+    private static int addChar(Map<Character, Integer> freq, char a, Function<Integer, Integer> aplicator) {
         int zeroes = 0;
         Integer prev = freq.get(a);
         if (prev != null) {
-            freq.put(a, prev - 1);
-            if (prev == 1) {
+            Integer newVal = aplicator.apply(prev);
+            freq.put(a, newVal);
+            if (newVal.equals(0)) {
                 ++zeroes;
-            } else if (prev == 0) {
+            } else if (prev.equals(0)) {
                 --zeroes;
             }
         }
