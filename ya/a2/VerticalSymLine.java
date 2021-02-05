134348a2cdf9c7fac598138da48f7647d167f48c
a.savanovich <asavanster@gmail.com>
2021-02-05 19:05:22 +0300

    sym line tests

--- ya/a2/VerticalSymLine.java
+++ ya/a2/VerticalSymLine.java
@@ -1,8 +1,13 @@
 package ya.a2;
 
+import org.junit.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by asavan on 05.02.2021.
@@ -50,6 +55,11 @@ public class VerticalSymLine {
             boolean res = hasVerticalSymLine(points);
             System.out.println(res);
         }
+        {
+            Point[] points = {};
+            boolean res = hasVerticalSymLine(points);
+            System.out.println(res);
+        }
     }
 
     private static boolean hasVerticalSymLine(Point[] points) {
@@ -68,12 +78,46 @@ public class VerticalSymLine {
             }
             Point key = new Point(2 * mid - point.x*2, point.y);
             Integer symPoint = allPoints.get(key);
-            if (symPoint == null) {
+            if (symPoint == null || symPoint.equals(0)) {
                 return false;
             } else {
                 allPoints.put(key, symPoint - 1);
             }
         }
         return true;
+    }
+
+    @Test
+    public void test() {
+        {
+            Point[] points = {new Point(0, 0), new Point(0, 0), new Point(1, 1), new Point(2, 2),
+                    new Point(3, 1), new Point(4, 0), new Point(4, 0)};
+            assertTrue(hasVerticalSymLine(points)); // true
+        }
+        {
+            Point[] points = {new Point(0, 0), new Point(0, 0), new Point(1, 1), new Point(2, 2),
+                    new Point(3, 1), new Point(4, 0)};
+            assertFalse(hasVerticalSymLine(points)); // false
+        }
+        {
+            Point[] points = {};
+            assertTrue(hasVerticalSymLine(points));
+        }
+        {
+            Point[] points = {new Point(0, 0)};
+            assertTrue(hasVerticalSymLine(points));
+        }
+        {
+            Point[] points = {new Point(0, 0), new Point(10, 0)};
+            assertTrue(hasVerticalSymLine(points));
+        }
+        {
+            Point[] points = {new Point(0, 0), new Point(11, 1)};
+            assertFalse(hasVerticalSymLine(points));
+        }
+        {
+            Point[] points = {new Point(0, 0), new Point(1, 0), new Point(3, 0)};
+            assertFalse(hasVerticalSymLine(points));
+        }
     }
 }
