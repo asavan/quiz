64f679abd533a38d5e296b227054ecde2657d20e
a.savanovich <asavanster@gmail.com>
2024-07-31 17:41:48 +0300

    delete pss

---
+++ ya/DeletePSS.java
@@ -0,0 +1,55 @@
+package ya;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeletePSS {
+    public static void main(String[] args) {
+        System.out.println(remove("ab()c"));
+    }
+
+    private static String remove(String s) {
+        var arr = s.toCharArray();
+        int newLen = removeInner(arr);
+        return new String(arr, 0, newLen);
+    }
+
+    private static int removeInner(char[] charArray) {
+        int curWrite = 0;
+        int openLen = 0;
+        for (var c: charArray) {
+            if (c == '(') {
+                ++openLen;
+            }
+            else if (c == ')') {
+                if (openLen > 0) {
+                    --openLen;
+                } else {
+                    charArray[curWrite++] = ')';
+                }
+            } else {
+                while (openLen > 0) {
+                    --openLen;
+                    charArray[curWrite++] = '(';
+                }
+                charArray[curWrite++] = c;
+            }
+        }
+        while (openLen > 0) {
+            --openLen;
+            charArray[curWrite++] = '(';
+        }
+        return curWrite;
+    }
+
+    @Test
+    public void test() {
+        assertEquals("abc", remove("ab()c"));
+        assertEquals("ab(a)", remove("ab(()a)"));
+        assertEquals("", remove("(((())))"));
+        assertEquals("", remove("(()())"));
+        assertEquals("(((", remove("(()(("));
+        assertEquals("(.)_(.)", remove("(.)_(.)"));
+    }
+}
