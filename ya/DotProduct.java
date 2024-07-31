96fb1c537ed459dd73751da73aa92edfdf3ffea2
a.savanovich <asavanster@gmail.com>
2024-07-31 19:12:44 +0300

    refactor

--- ya/DotProduct.java
+++ ya/DotProduct.java
@@ -1,7 +1,11 @@
 package ya;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class DotProduct {
     static class Node {
@@ -17,6 +21,14 @@ public class DotProduct {
         }
     }
 
+    private static int len(List<Node> list) {
+        int result = 0;
+        for (var n: list) {
+            result += n.count;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         var list = new ArrayList<Node>();
         list.add(new Node(1, 3));
@@ -27,43 +39,51 @@ public class DotProduct {
     }
 
     private static int dotProduct(List<Node> list, List<Node> list2) {
+        if (len(list) != len(list2)) {
+            throw new IllegalArgumentException("Bad lists");
+        }
+
         if (list.isEmpty() || list2.isEmpty()) {
             return 0;
         }
         int ind1 = 0;
         int ind2 = 0;
         int result = 0;
+        int rest = len(list);
         var node1 = list.get(ind1).clone();
         var node2 = list2.get(ind2).clone();
-        while (true) {
-            if (node1.count < node2.count) {
-                node2.count -= node1.count;
-                result += node1.value * node2.value * node1.count;
+        while (rest > 0) {
+            int diff = Math.min(node1.count, node2.count);
+            result += node1.value * node2.value * diff;
+            node1.count -= diff;
+            node2.count -= diff;
+            rest -= diff;
+            if (node1.count == 0) {
                 ++ind1;
                 if (ind1 >= list.size()) {
                     break;
                 }
                 node1 = list.get(ind1).clone();
-            } else if (node1.count > node2.count) {
-                node1.count -= node2.count;
-                result += node1.value * node2.value * node2.count;
+            }
+            if (node2.count == 0) {
                 ++ind2;
                 if (ind2 >= list2.size()) {
                     break;
                 }
                 node2 = list2.get(ind2).clone();
-            } else {
-                ++ind1;
-                ++ind2;
-                result += node1.value * node2.value * node1.count;
-                if(ind1 >= list.size() || ind2 >= list2.size()) {
-                    break;
-                }
-                node1 = list.get(ind1).clone();
-                node2 = list2.get(ind2).clone();
             }
-
         }
         return result;
+    }
+
+    @Test
+    public void test() {
+        var list = new ArrayList<Node>();
+        list.add(new Node(1, 3));
+        var list2 = new ArrayList<Node>();
+        list2.add(new Node(1, 2));
+        list2.add(new Node(10, 1));
+
+        assertEquals(12, dotProduct(list, list2));
     }
 }
