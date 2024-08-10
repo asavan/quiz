a4d22e1b0ed025e72d50e3916aea474232a44f41
a.savanovich <asavanster@gmail.com>
2024-08-10 22:56:47 +0300

    paths in matrix

--- ya/PathsInMatrix.java
+++ ya/PathsInMatrix.java
@@ -1,35 +1,15 @@
 package ya;
 
+// https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/
+
 public class PathsInMatrix {
-    private static final int MODULO = (int)1e9 + 7;
-
-    int getDiff(int value, int k) {
-        return value % k;
-    }
-
-    int[] newArr(int[] prev, int diff) {
-        int k = prev.length;
-        int[] cur = new int[k];
-        for (int i = 0; i < k; ++i) {
-            cur[(i+diff)%k] = prev[i];
-        }
-        return cur;
-    }
+    private static final int MODULO = (int) 1e9 + 7;
 
     void newArr2(int[] cur, int[] prev, int diff) {
         int k = prev.length;
         for (int i = 0; i < k; ++i) {
-            cur[(i+diff)%k] = prev[i];
+            cur[(i + diff) % k] = prev[i];
         }
-    }
-
-    int[] mergeArr(int[] l1, int[] l2) {
-        int k = l1.length;
-        int[] cur = new int[k];
-        for (int i = 0; i < k; ++i) {
-            cur[i] = (l1[i] + l2[i]) % MODULO;
-        }
-        return cur;
     }
 
     void mergeArr2(int[] cur, int[] l1, int[] l2) {
@@ -39,28 +19,26 @@ public class PathsInMatrix {
         }
     }
 
-    void fillFirst(int[][] l1, int n, int[][] grid, int k) {
-        int[] prev = new int[k];
-        prev[0] = 1;
-        newArr2(l1[0], prev, getDiff(grid[0][0], k));
-        for (int i = 1; i < n; ++i) {
-            newArr2(l1[i], l1[i-1], getDiff(grid[0][i], k));
+    int[][] fillLine(int[][] levelPrev, int n, int[][] grid, int k, int line) {
+        var levelCur = new int[n][k];
+        newArr2(levelCur[0], levelPrev[0], grid[line][0]);
+        for (int in = 1; in < n; ++in) {
+            int diff = grid[line][in];
+            newArr2(levelCur[in], levelCur[in - 1], diff);
+            newArr2(levelPrev[in - 1], levelPrev[in], diff);
+            mergeArr2(levelCur[in], levelCur[in], levelPrev[in - 1]);
         }
+        return levelCur;
     }
+
     public int numberOfPaths(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        var levelPrev = new int[n][k];
-        var levelCur = new int[n][k];
-        fillFirst(levelPrev, n, grid, k);
-        for (int i = 1; i < m; ++i) {
-            levelCur[0] = newArr(levelPrev[0], getDiff(grid[i][0], k));
-            for (int in = 1; in < n; ++in) {
-                int diff = getDiff(grid[i][in], k);
-                levelCur[in] = mergeArr(newArr(levelCur[in-1], diff), newArr(levelPrev[in], diff));
-            }
-            levelPrev = levelCur;
+        var level = new int[n][k];
+        level[0][0] = 1;
+        for (int i = 0; i < m; ++i) {
+            level = fillLine(level, n, grid, k, i);
         }
-        return levelPrev[n-1][0];
+        return level[n - 1][0];
     }
 }
